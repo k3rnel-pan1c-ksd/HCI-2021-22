c816d42c9b35bd4b9400ac7274b701a8324b480d
DariOS <dariosgit@gmail.com>
2021-10-21 11:20:22 +0200

    Add navbar elements and logo image

---
+++ pages/home.js
@@ -0,0 +1,17 @@
+/*function Home(props) {
+    return (
+        <div>
+            <h1>Home sweet Home</h1>
+        </div>
+    );
+}*/
+
+import Header from "../modules/header/header";
+
+const Home = () => (
+  <>
+    <Header />
+  </>
+);
+
+export default Home;
